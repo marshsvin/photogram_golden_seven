2d91051dcc95c888ceb527eb7a58b1e496549035
Alex Menkhaus <alex.menkhaus@gmail.com>
2017-04-26 00:41:23 -0500

    final

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,16 +1,50 @@
 class PhotosController < ApplicationController
 
 def show
-p = Photo.find(params["the_id_number"])
-@the_image_url = p.source
-@caption = p.caption
-@created_at = p.created_at
+@photo = Photo.find(params["the_id_number"])
 render("show.html.erb")
 end
 
 def index
-@all_photos = Photo.all
+@all_photos = Photo.order({:created_at => :desc}).all
 render("index.html.erb")
+end
+
+def new_form
+render("new_form.html.erb")
+end
+
+def create_row
+#The params look like this:
+#Parameters: {"the_image_url"=>"https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_120x44dp.png", #"the_caption"=>"google logo"}
+
+p = Photo.new
+p.source = params["the_image_url"]
+p.caption = params["the_caption"]
+p.save
+redirect_to("http://localhost:3000/photos")
+end
+
+def edit_form
+  @the_photo_to_be_edited = Photo.find(params["the_id"])
+
+render("edit_form.html.erb")
+end
+
+def update_row
+p = Photo.find(params[:the_id])
+p.source = params[:the_source]
+p.caption = params[:the_caption]
+p.save
+@url = "/photos/"+params[:the_id].to_s
+redirect_to(@url)
+end
+
+def delete_photo
+p = Photo.find(params[:the_id])
+p.destroy
+p.save
+redirect_to("/photos")
 end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,20 @@
 Rails.application.routes.draw do
 
-  get("/photos/:the_id_number", { :controller => "photos", :action => "show"})
+  #Create
+  get("/photos/new", {:controller => "photos", :action => "new_form"})
+  get("/create_photo", {:controller => "photos", :action => "create_row"})
 
+  #Read
+  get("/photos/:the_id_number", { :controller => "photos", :action => "show"})
   get("/photos", {:controller => "photos", :action => "index"})
-  
+
+  #Update
+  get("/photos/:the_id/edit", {:controller => "photos", :action => "edit_form"})
+  get("update_photo/:the_id", {:controller => "photos", :action => "update_row"})
+
+  #Delete
+  get("/delete_photo/:the_id", {:controller => "photos", :action => "delete_photo"})
+
   #devise_for :admin_users, ActiveAdmin::Devise.config
   #ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
